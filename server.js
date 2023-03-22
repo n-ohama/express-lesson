a1730d2629a6316e34b700c6e57e9a1ea5015cf8
enu <enu1996502@gmail.com>
2023-03-23 05:34:46 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const mongoose = require("mongoose");
+const Thread = require("./models/Thread");
+
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://nagatomo:bokeno0810@cluster0.9eyyiv3.mongodb.net/threads?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
+
+app.get("/api/v1/threads", async (req, res) => {
+  try {
+    const allThreads = await Thread.find({});
+    res.status(200).json(allThreads);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/api/v1/thread", async (req, res) => {
+  try {
+    const createThread = await Thread.create(req.body);
+    res.status(200).json(createThread);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(PORT, console.log("server running"));
